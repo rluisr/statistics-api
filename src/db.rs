80732e5482f32be3eefef4551f760aac5d7cba94
hasegawa_takuya_xa <hasegawa_takuya_xa@cyberagent.co.jp>
2019-07-30 18:16:22 +0900

    :recycle: Refactoring         Require env TS

--- src/db.rs
+++ src/db.rs
@@ -3,5 +3,5 @@ use mysql;
 static DB_URL: &'static str = env!("DB_URL");
 
 pub fn connect() -> mysql::Pool {
-    return mysql::Pool::new(DB_URL).unwrap();
+    mysql::Pool::new(DB_URL).unwrap();
 }
